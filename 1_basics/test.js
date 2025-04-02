f92335a277c023fd461548686d6a5eca269f6a7f
Singhcr07 <shikharsinghcr07@gmail.com>
2025-04-02 09:38:23 +0000

    latest changes text

---
+++ 1_basics/test.js
@@ -0,0 +1,8 @@
+const accountId = 123454
+let accountEmail = "Singh07@yopmail.com"
+var accountPassword = "Singh"
+accountCity = "CHD"
+
+let accountState;
+console.table({ accountId, accountEmail, accountPassword, accountCity, accountState });
+
